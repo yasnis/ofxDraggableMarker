6a93f437cda59a9694b14a25a2d9fc98c6dad6a6
LAPTOP-83IF6KVB\develop <yasuhiko.nishimura@gmail.com>
2016-12-27 02:49:52 +0900

    Re add setOffset method.

--- src/ofxDraggableMarker.h
+++ src/ofxDraggableMarker.h
@@ -11,28 +11,42 @@
 
 #include "ofMain.h"
 
-class ofxDraggableMarker: public ofVec2f {
-private:
-    ofVec2f offset;
-    bool dragging;
-    ofRectangle rect;
-    float radius = 14.0;
-    ofColor color;
+class ofxDraggableMarker : public ofVec2f {
+protected:
+	bool isPress = false;
+	bool isHover = false;
 
-    bool isPress = false;
-    bool isHover = false;
-    
-    int id;
-    
+private:
+	ofVec2f offset;
+	ofVec2f dist;
+	bool dragging;
+	ofRectangle rect;
+	ofColor color;
+
+	void drawCircle(const ofPoint &p, float r);
+
+protected:
+	float radius = 14.0;
+	int id;
+	string label = "";
+
 public:
-    ofxDraggableMarker();
-    ofxDraggableMarker(ofVec2f pos, int i, ofColor c = ofColor(255, 0, 0));
-    
-    void draw();
-    
-    void setID(int _id);
-    int getID();
-    
+	ofxDraggableMarker();
+	ofxDraggableMarker(ofVec2f pos, int i, ofColor c = ofColor(255, 0, 0));
+	ofxDraggableMarker(ofVec2f pos, string label, ofColor c = ofColor(255, 0, 0));
+
+	virtual void draw();
+	virtual void draw(float x, float y);
+
+	virtual void setID(int _id);
+	virtual int getID();
+
+	virtual void setLabel(string _label);
+	virtual string getLabel();
+
+	virtual void setRadius(float _radius);
+	virtual float getRadius();
+
 
 	void mouseReleased(ofMouseEventArgs & args);
 	void mouseMoved(ofMouseEventArgs & args);
@@ -41,11 +55,15 @@ public:
 	void mouseScrolled(ofMouseEventArgs & args);
 	void mouseEntered(ofMouseEventArgs & args);
 	void mouseExited(ofMouseEventArgs & args);
-    
-    
-    void setColor(ofColor col);
-    ofColor getColor();
-    
+
+
+
+	virtual void setColor(ofColor col);
+	virtual ofColor getColor();
+
+	void setOffset(ofVec2f v);
+	ofVec2f getOffset();
+
 };
 
 #endif /* defined(__ofApp__ofxDragMarker__) */
